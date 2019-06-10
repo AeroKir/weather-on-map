afb9b627c3b125d342a7ce4e7185799ca477e7f0
Kirill Shevtsov <kirwork@google.com>
2019-06-10 20:20:22 +0300

    Add constants.js

---
+++ src/constants.js
@@ -0,0 +1,5 @@
+/**
+ * Constants for working with the OpenWeatherMap API
+ */
+export const BASE_API_URL = 'http://api.openweathermap.org/data/2.5/';
+export const APP_ID = 'APPID=863a0565129f6b0538ef1729aea12f4f';
