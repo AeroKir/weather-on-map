a9c23ebf59c26076f135f933389c7dbeebd4d193
Kirill Shevtsov <kirwork@google.com>
2019-06-11 18:48:08 +0300

    Removing unnecessary imports

--- src/index.js
+++ src/index.js
@@ -1,17 +1,4 @@
-// import map from './map-components/init-map';
 import weatherView from './view/view-on-map';
-// import weatherRequest from './api-components/weather-request';
-// import dataHandler from './api-components/data-handler';
-
-// Test init map. Remove before production
-// import map from './tests/test-init-map';
-// import map from './tests/test-forecast-map';
-// import map from './tests/test-init-multiple-items';
-// import forecastHandler from './tests/test-forecast-without-map';
-// import ukrainianCities from './helper-functions/get-ukrainian-sities';
-
-// import responseObject from './api-components/response-parse';
-// import responseObject from './api-components/weather-request';
 
 import './leaflet.css';
 import './main.css';
