1ec11e395ab789d38fa71f78b0dc74267606fca4
Kirill Shevtsov <kirwork@google.com>
2019-06-12 16:04:40 +0300

    Adding leaflet.css import and changing GEO API

--- src/map-components/init-map.js
+++ src/map-components/init-map.js
@@ -1,20 +1,22 @@
 import L from 'leaflet';
 import openStreetMap from './tile-layers';
+import { GEO_API_URL, GEO_API_KEY } from '../constants';
 
-import './weather-popup.css';
-import './thumbnail-marker.css';
+import '../leaflet.css';
 
 const map = L.map('map', {
+  center: [48.153719, 24.822922],
   zoom: 13,
   layers: [openStreetMap],
   worldCopyJump: true,
 });
 
 // Set initial state of the Map
-const fetchRequest = fetch('http://api.ipstack.com/check?access_key=ae0911201b26609e947dba212bfe8a9f')
+const fetchRequest = fetch(`${GEO_API_URL}?apiKey=${GEO_API_KEY}`)
   .then(coordinates => coordinates.json())
   .then((myJson) => {
-    const userCoordinates = new L.LatLng(myJson.latitude, myJson.longitude);
+    const userCoordinates = new L.LatLng(myJson.location.lat, myJson.location.lng);
+
     return userCoordinates;
   })
   .then((userCoordinates) => {
