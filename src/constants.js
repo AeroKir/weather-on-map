165f4ca6beead692c27049397f9de90ec49a9992
Kirill Shevtsov <kirwork@google.com>
2019-06-12 15:58:43 +0300

    Add new constants for geo.ipify.org API

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,11 @@
 /**
  * Constants for working with the OpenWeatherMap API
  */
-export const BASE_API_URL = 'http://api.openweathermap.org/data/2.5/';
+export const BASE_API_URL = 'https://api.openweathermap.org/data/2.5/';
 export const APP_ID = 'APPID=863a0565129f6b0538ef1729aea12f4f';
+
+/**
+ * Constants for working with the https://geo.ipify.org/ API
+ */
+export const GEO_API_URL = 'https://geo.ipify.org/api/v1';
+export const GEO_API_KEY = 'at_NPARPjcocCWDfodwCJ5ZoGArS81HH';
